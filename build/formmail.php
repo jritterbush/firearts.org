859d11c4c3289722905c707ab8ae9819a931d4f6
John Ritterbush <jritterbush@gmail.com>
2016-08-06 10:41:44 -0500

    Clean up
    - move files to build directory for easier migration to build tools
    - update head content to be a tad more modern
    - fix some layout issues
    - move header styles into external stylesheet
    - change home links from index.html to /
    - remove spacer.gif
    - remove links to aboutsite.html
    - remove files that are being redirected to other pages

---
+++ build/formmail.php
@@ -0,0 +1,84 @@
+<?php include('includes/constants.php'); ?>
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>fire arts center of chicago | online class registration form</title>
+	<meta name="description" content="Fill out our online form to register for exciting classes at the fire arts center of chicago">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="/style.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+
+	<!-- Google Tag Manager -->
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5BNZSL"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-5BNZSL');</script>
+	<!-- End Google Tag Manager -->
+	
+<table class="main" align="center" border="0" cellpadding="0" cellspacing="0">
+	<tr>
+    	<td class="leftheader"><a href="/"><img src="/Images/vitruvianman.jpg" title="fire arts center of chicago | metal sculpture and design" alt="fire arts center of chicago | metal sculpture and design"></a></td>
+      	<td class="banner"><a href="/"><img src="/Images/banner.jpg" title="fire arts center of chicago | metal sculpture and design" alt="fire arts center of chicago | metal sculpture and design"></a></td>
+    </tr>
+    <tr>
+      	<td class="navbarbase">
+      		<table class="navbar" align="center" border="0" cellpadding="0" cellspacing="0">
+            	<tr>
+            		<td class="navbarcellmain"><span class="class1"><a href="/">home</a></span></td>
+          		</tr>
+          		<tr>
+                	<td class="navbarcellmain"><span class="class1"><a href="classes.html">classes</a></span></td>
+          		</tr>
+          		<tr>
+            		<td class="navbarcellmain"><span class="class1"><a href="gallery.html">gallery</a></span></td>
+          		</tr>
+          		<tr>
+            		<td class="navbarcellmain"><span class="class1"><a href="events.html">events</a></span></td>
+              	</tr>
+              	<tr>
+                	<td class="navbarcellmain"><span class="class1"><a href="links.html">links</a></span></td>
+              	</tr>
+              	<tr>
+                	<td class="navbarcellmain"><span class="class1"><a href="contactus.html">contact us</a></span></td>
+              	</tr>
+          		<tr>
+            		<td class="navbarcell2"><span class="current2">registration form</span></td>
+          		</tr>
+          		<tr>
+            		<td class="navbarcellmain"><span class="class1"><a href="donate.html">help out</a></span></td>
+         		 </tr>
+             </table>
+      	</td>
+      	<td style="vertical-align:top;">
+      		<table style="width: 598px; text-align: left;" valign="top" align="center" border="0" cellpadding="4" cellspacing="4">
+                <tr>
+                    <td class="pageindicator" style="vertical-align:top;"><div class="header1">class registration form</div></td>
+                </tr>
+                <tr>
+                    <td colspan="3"><span class="header3" align="center">Under Construction....</span>
+                    <p>Form currently under contsruction. In the meantime please fill out a <a href="/FireArts_RegistrationForm.pdf" target="_blank">pdf version</a> of the form, or call <?php echo $phone; ?> for more information.</p></td>
+                </tr>
+      		</table>
+      	</td>
+    </tr>
+    <tr>
+      	<td class="bottomleft"></td>
+      	<td align="center"><hr style="width: 100%; height: 2px;"><span class="footer"><?php echo $organization . ' | ' . $address . ' | ' . $city . ' ' . $state . ', ' . $zip . '<br> Tel: ' . $phone . ' | Tel2: ' . $phone2 . ' | Email: <a href="mailto:' . $email . '">' . $email . '</a>'; ?></span></td>
+    </tr>
+    <tr>
+      	<td class="bottomleft"></td>
+      	<td align="center"><div class="tiny"><a href="sitemap.html">Sitemap</a></div></td>
+    </tr>
+</table>
+
+</body>
+</html>
+
