147fdb2529308754b5a1532d8a1ecbd699d1f5e6
John Ritterbush <jritterbush@gmail.com>
2017-12-31 17:07:04 -0600

    Node and gulp updates
    - clear out old content from node packages
    - add in tools to manage normalize css file

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+
+gulp.task('copy', () => {
+    gulp.src('node_modules/normalize.css/normalize.css')
+        .pipe(rename('_normalize.scss'))
+        .pipe(gulp.dest('./_sass/'));
+});
