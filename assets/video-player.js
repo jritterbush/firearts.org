5d2141125ac989c68a4fe679b43b2c4bd88e75a8
John Ritterbush <jritterbush@gmail.com>
2018-01-28 11:10:53 -0600

    Add video loading script instead of embedded iframe

---
+++ assets/video-player.js
@@ -0,0 +1,22 @@
+function ready(fn) {
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+const videoUrl = 'http://www.vimeo.com/21648751';
+const endpoint = 'http://www.vimeo.com/api/oembed.json';
+const callback = 'embedVideo';
+const url = endpoint + '?url=' + encodeURIComponent(videoUrl) + '&callback=' + callback + '&width=550';
+function embedVideo(video) {
+    document.getElementById('embed').innerHTML = unescape(video.html);
+}
+function init() {
+    const js = document.createElement('script');
+    js.setAttribute('type', 'text/javascript');
+    js.setAttribute('src', url);
+    document.getElementsByTagName('head').item(0).appendChild(js);
+}
+ready(init);
