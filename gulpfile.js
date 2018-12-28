7d80932bbe14044e0dd21fcf3c896d6808e1ff6e
John Ritterbush <jritterbush@gmail.com>
2018-12-27 20:13:54 -0600

    fix: add postcss tools and build steps

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,41 @@
 'use strict';
 
-const bs     = require('browser-sync').create();
-const child  = require('child_process');
-const gulp   = require('gulp');
-const gutil  = require('gulp-util');
-const rename = require('gulp-rename');
+const bs       = require('browser-sync').create();
+const child    = require('child_process');
+const gulp     = require('gulp');
+const gutil    = require('gulp-util');
+const postcss  = require('gulp-postcss');
+const pcColor  = require('postcss-color-function');
+const pcImport = require('postcss-import');
+const pcNested = require('postcss-nested');
+const rename   = require('gulp-rename');
+const tailwind = require('tailwindcss');
 
-const siteRoot = './_site';
+const siteRoot       = './_site';
+const cssRoot        = `${siteRoot}/assets/css/`;
+const cssTemp        = './_includes/';
+const stylesheet     = './src/styles/styles.scss';
+const tailwindConfig = './tailwind.config.js';
+const devBuild       = (process.env.NODE_ENV || 'development').trim().toLowerCase() === 'development';
 
 gulp.task('copy', () => {
     gutil.log('Copying normalize to vendor directory');
     return gulp.src('node_modules/normalize.css/normalize.css')
         .pipe(rename('_normalize.scss'))
-        .pipe(gulp.dest('./_sass/vendor/'));
+        .pipe(gulp.dest('./src/styles/vendor/'));
+});
+
+gulp.task('styles', () => {
+    gutil.log('Building styles');
+    return gulp.src(stylesheet)
+        .pipe(postcss([
+            pcImport(),
+            pcNested(),
+            tailwind(tailwindConfig),
+            pcColor(),
+        ]))
+        .pipe(rename('styles.css'))
+        .pipe(gulp.dest(cssTemp))
 });
 
 gulp.task('jekyll', (cb) => {
@@ -39,8 +62,9 @@ gulp.task('serve', () => {
             baseDir: siteRoot
         }
     });
-    gulp.watch('content/**', gulp.series(['jekyll']));
+    gulp.watch(['_includes/**/*.html', '_layouts/**/*.html', 'content/**'], gulp.series(['jekyll']));
+    gulp.watch(['tailwind.config.js', 'src/styles/**'], gulp.series(['styles', 'jekyll']));
 });
 
-gulp.task('default', gulp.series(['copy', 'jekyll', 'serve']));
-gulp.task('build', gulp.series(['copy', 'jekyll']));
+gulp.task('default', gulp.series(['styles', 'jekyll', 'serve']));
+gulp.task('build', gulp.series(['styles', 'copy', 'jekyll']));
