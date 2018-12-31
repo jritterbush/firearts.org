c5a792b2604094ffa4510a61ff2a612bb02aa883
John Ritterbush <jritterbush@gmail.com>
2018-12-30 18:09:30 -0600

    feat: set up purgecss and cleancss to cut back on tailwind css size

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,11 @@ const child    = require('child_process');
 const gulp     = require('gulp');
 const gutil    = require('gulp-util');
 const postcss  = require('gulp-postcss');
+const pcClean  = require('postcss-clean');
 const pcColor  = require('postcss-color-function');
 const pcImport = require('postcss-import');
 const pcNested = require('postcss-nested');
+const purgecss = require('gulp-purgecss');
 const rename   = require('gulp-rename');
 const tailwind = require('tailwindcss');
 
@@ -18,12 +20,12 @@ const stylesheet     = './src/styles/styles.scss';
 const tailwindConfig = './tailwind.config.js';
 const devBuild       = (process.env.NODE_ENV || 'development').trim().toLowerCase() === 'development';
 
-gulp.task('copy', () => {
-    gutil.log('Copying normalize to vendor directory');
-    return gulp.src('node_modules/normalize.css/normalize.css')
-        .pipe(rename('_normalize.scss'))
-        .pipe(gulp.dest('./src/styles/vendor/'));
-});
+// Custom extractor for purgeCSS, to avoid stripping classes with `:` prefixes
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
 
 gulp.task('styles', () => {
     gutil.log('Building styles');
@@ -33,7 +35,17 @@ gulp.task('styles', () => {
             pcNested(),
             tailwind(tailwindConfig),
             pcColor(),
+            pcClean(),
         ]))
+        .pipe(purgecss({
+            content: ['_includes/**/*.html', '_layouts/**/*.html', 'content/**/*.html', '_data/**/*.yml'],
+            extractors: [
+                {
+                    extractor: TailwindExtractor,
+                    extensions: ['html', 'js', 'yml']
+                }
+            ]
+        }))
         .pipe(rename('styles.css'))
         .pipe(gulp.dest(cssTemp))
 });
@@ -67,4 +79,4 @@ gulp.task('serve', () => {
 });
 
 gulp.task('default', gulp.series(['styles', 'jekyll', 'serve']));
-gulp.task('build', gulp.series(['styles', 'copy', 'jekyll']));
+gulp.task('build', gulp.series(['styles', 'jekyll']));
